4bbf69cf15187a675ca542056cb3daac287b7292
gabrielfirmo <gabriel_firmo@hotmail.com>
2021-05-16 16:21:45 -0300

    coordenadas das cidades

--- src/main/java/com/github/gabrielfirmo/citiesapi/cities/City.java
+++ src/main/java/com/github/gabrielfirmo/citiesapi/cities/City.java
@@ -4,12 +4,15 @@ import com.github.gabrielfirmo.citiesapi.states.State;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "cidade")
-
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
 public class City {
 
     @Id
@@ -29,11 +32,23 @@ public class City {
     @Column(name = "lat_lon")
     private String geolocation;
 
+    // 2nd
+    @Type(type = "point")
+    @Column(name = "lat_lon", updatable = false, insertable = false)
+    private Point location;
 
     public City() {
     }
 
-
+    /*public City(final Long id, final String name, final Integer uf, final Integer ibge,
+                final String geolocation, final Point location) {
+        this.id = id;
+        this.name = name;
+        this.uf = uf;
+        this.ibge = ibge;
+        this.geolocation = geolocation;
+        this.location = location;
+    }*/
 
     public Long getId() {
         return id;
@@ -43,7 +58,9 @@ public class City {
         return name;
     }
 
-
+    /*public Integer getUf() {
+        return uf;
+    }*/
 
     public State getState() {
         return state;
@@ -57,5 +74,7 @@ public class City {
         return geolocation;
     }
 
-
+    public Point getLocation() {
+        return location;
+    }
 }
